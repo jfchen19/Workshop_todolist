a3d6758f9b5ec4c227cebb8c28b3327ad905c140
jfchen19 <jchenf19@gmail.com>
2020-12-08 12:46:25 +0800

    addCheck complete

--- js-todo-list/main.js
+++ js-todo-list/main.js
@@ -1,12 +1,30 @@
 // TO DO
 // 新增/移除 checked class，只有第一個li有動作
 window.addEventListener('DOMContentLoaded', function() {
-  document.querySelector('ul li').addEventListener('click', function() {
-    const li = document.querySelector('ul li').classList.toggle('checked')
-  })
+  document.querySelector('ul li').addEventListener('click', addCheck())
 })
 
+function addCheck(){
+  let row = document.querySelectorAll('ul li')
+  for (let i=0; i<row.length; i++){
+    row[i].addEventListener('click', function() {
+      row[i].classList.toggle("checked")
+    })
+  }
+}
 
+
+
+
+
+
+
+
+
+//  點擊Ｘ刪除任務
+document.addEventListener('DOMContentLoaded', function() {
+
+})
 
 
 
