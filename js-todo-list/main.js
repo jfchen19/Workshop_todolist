6892f4d89add182985852561821a46c67916d38d
jfchen19 <jchenf19@gmail.com>
2020-12-08 12:13:45 +0800

    Complete_task_add_checked

--- js-todo-list/main.js
+++ js-todo-list/main.js
@@ -1,1 +1,28 @@
 // TO DO
+// 新增/移除 checked class，只有第一個li有動作
+window.addEventListener('DOMContentLoaded', function() {
+  document.querySelector('ul li').addEventListener('click', function() {
+    const li = document.querySelector('ul li').classList.toggle('checked')
+  })
+})
+
+
+
+
+
+
+
+
+
+
+// ＋
+// document.addEventListener('DOMContentLoaded', function () {
+//   document.querySelector('.addBtn').addEventListener('click', function() {
+//     document.querySelector('ul').addEventListener('beforeend', addRow())
+//   })
+// })
+
+// function addRow() {
+//   let div = document.createElement('div')
+//   div.
+// }
